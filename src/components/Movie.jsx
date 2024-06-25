29d0a03973fd095b9290e981d0260961f7f70f03
tame84 <tame.disc@gmail.com>
2024-06-25 13:18:02 +0200

    ajout de la page "favoris". Affichage des favoris. Action pour retirer un favoris

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Movie = ({ id, img, title, release, notation, synopsis }) => {
+const Movie = ({ id, img, title, release, notation, synopsis, favorite }) => {
     const addFavorite = () => {
         if (window.localStorage.favoriteMovies) {
             const favoriteMovies =
@@ -19,6 +19,20 @@ const Movie = ({ id, img, title, release, notation, synopsis }) => {
         }
     };
 
+    const removeFavorite = (e) => {
+        if (window.localStorage.favoriteMovies) {
+            let favoriteMovies = window.localStorage.favoriteMovies.split(",");
+
+            favoriteMovies = favoriteMovies.filter((movie) => {
+                return movie != id;
+            });
+
+            window.localStorage.favoriteMovies = favoriteMovies.join(",");
+        }
+
+        e.target.parentElement.style.display = "none";
+    };
+
     return (
         <div className="movie-card">
             <div className="img-container">
@@ -30,11 +44,19 @@ const Movie = ({ id, img, title, release, notation, synopsis }) => {
             </div>
             <h2>{title}</h2>
             <ul className="infos">
-                <li>{notation}</li>
-                <li>{release}</li>
+                <li>{notation && notation}</li>
+                <li>{release && release}</li>
             </ul>
             <p className="synopsis">{synopsis}</p>
-            <button onClick={() => addFavorite()}>Ajouter aux favoris</button>
+            {favorite ? (
+                <button onClick={(e) => removeFavorite(e)}>
+                    Retirer des favoris
+                </button>
+            ) : (
+                <button onClick={() => addFavorite()}>
+                    Ajouter aux favoris
+                </button>
+            )}
         </div>
     );
 };
--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -4,21 +4,25 @@ import Movie from "../components/Movie";
 import axios from "axios";
 
 const Favorites = () => {
+    const favoriteMovies = window.localStorage.favoriteMovies.split(",");
     const [moviesData, setMoviesData] = useState([]);
 
     useEffect(() => {
         if (window.localStorage.favoriteMovies) {
-            const moviesIds = window.localStorage.favoriteMovies.split(",");
-
-            moviesIds.map((id) => {
-                axios.get(
-                    `https://api.themoviedb.org/3/movie/${id}?api_key=${
-                        import.meta.env.VITE_IMBD_API_KEY
-                    }&language=fr-FR`
-                );
+            Promise.all(
+                favoriteMovies.map((id) =>
+                    axios.get(
+                        `https://api.themoviedb.org/3/movie/${id}?api_key=${
+                            import.meta.env.VITE_IMBD_API_KEY
+                        }&language=fr-FR`
+                    )
+                )
+            ).then((res) => {
+                const movies = res.map((res) => res.data);
+                setMoviesData(movies);
             });
         }
-    }, []);
+    }, [favoriteMovies]);
 
     return (
         <div>
@@ -27,13 +31,14 @@ const Favorites = () => {
                 {moviesData &&
                     moviesData.map((movie, index) => (
                         <Movie
-                            key={movie.title + "-index" + index}
+                            key={`${movie.title}-index${index}`}
                             id={movie.id}
                             img={movie.poster_path}
                             title={movie.title}
                             release={movie.release_date}
                             notation={movie.vote_average}
                             synopsis={movie.overview}
+                            favorite={true}
                         />
                     ))}
             </div>
